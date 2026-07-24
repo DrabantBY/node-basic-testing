5a2008fa0dd6de23c99a1d5fea28ae4eee96645a
DrabantBY <yauheni.basalai@gmail.com>
2026-07-24 22:27:29 +0300

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,60 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { Action, simpleCalculator } from './index';
+
+const a = 4;
+const b = 2;
+const action = '%';
 
 describe('simpleCalculator tests', () => {
-  test('should add two numbers', () => {
-    // Write your test here
+  test.each([
+    {
+      a,
+      b,
+      message: 'should add two numbers',
+      action: Action.Add,
+      expected: 6,
+    },
+    {
+      a,
+      b,
+      message: 'should subtract two numbers',
+      action: Action.Subtract,
+      expected: 2,
+    },
+    {
+      a,
+      b,
+      message: 'should multiply two numbers',
+      action: Action.Multiply,
+      expected: 8,
+    },
+    {
+      a,
+      b,
+      message: 'should divide two numbers',
+      action: Action.Divide,
+      expected: 2,
+    },
+    {
+      a,
+      b,
+      message: 'should exponentiate two numbers',
+      action: Action.Exponentiate,
+      expected: 16,
+    },
+  ])('$message', ({ a, b, action, expected }) => {
+    expect(simpleCalculator({ a, b, action })).toBe(expected);
   });
 
-  test('should subtract two numbers', () => {
-    // Write your test here
-  });
-
-  test('should multiply two numbers', () => {
-    // Write your test here
-  });
-
-  test('should divide two numbers', () => {
-    // Write your test here
-  });
-
-  test('should exponentiate two numbers', () => {
-    // Write your test here
-  });
-
-  test('should return null for invalid action', () => {
-    // Write your test here
-  });
-
-  test('should return null for invalid arguments', () => {
-    // Write your test here
+  test.each([
+    { a, b, message: 'should return null for invalid action', action },
+    {
+      a: '4',
+      b: '2',
+      message: 'should return null for invalid arguments',
+      action: Action.Add,
+    },
+  ])('$message', ({ a, b, action }) => {
+    expect(simpleCalculator({ a, b, action })).toBeNull();
   });
 });
